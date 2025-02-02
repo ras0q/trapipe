797af070fff1a9f842d75e6312d6cda79a60beac
ras0q <ras0q@users.noreply.github.com>
2025-02-02 13:34:29 +0900

    parse template

--- main.go
+++ main.go
@@ -1,24 +1,32 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
+	"text/template"
 
 	"github.com/alecthomas/kong"
 	traqwsbot "github.com/traPtitech/traq-ws-bot"
 	"github.com/traPtitech/traq-ws-bot/payload"
 )
 
-var CLI struct {
+var cli struct {
 	AccessToken string `help:"BOT Access Token" env:"TRAQ_BOT_ACCESS_TOKEN"`
 	WSOrigin    string `help:"traQ Websocket Origin" default:"wss://q.trap.jp" env:"TRAQ_WS_ORIGIN"`
+	Template    string `help:"Output Template (See https://pkg.go.dev/text/template)" default:"{{ .Message.PlainText }}" short:"t"`
 }
 
 func main() {
-	ctx := kong.Parse(&CLI)
+	ctx := kong.Parse(&cli)
 	bot, err := traqwsbot.NewBot(&traqwsbot.Options{
-		AccessToken: CLI.AccessToken,
-		Origin:      CLI.WSOrigin,
+		AccessToken: cli.AccessToken,
+		Origin:      cli.WSOrigin,
 	})
+	if err != nil {
+		panic(err)
+	}
+
+	tmpl, err := template.New("output").Parse(cli.Template)
 	if err != nil {
 		panic(err)
 	}
@@ -28,8 +36,13 @@ func main() {
 	})
 
 	bot.OnMessageCreated(func(p *payload.MessageCreated) {
-		// TODO: 将来的にtemplateにした方が嬉しそう
-		fmt.Println(p.Message.PlainText)
+		var output bytes.Buffer
+		if err := tmpl.Execute(&output, p); err != nil {
+			ctx.Errorf("execute template: %s", err.Error())
+			return
+		}
+
+		fmt.Println(output.String())
 	})
 
 	if err := bot.Start(); err != nil {
