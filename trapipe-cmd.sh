83952999da05bb30187c500708dc12caa77ff6b1
ras0q <ras0q@users.noreply.github.com>
2025-02-14 21:12:01 +0900

    remove unused

--- trapipe-cmd.sh
+++
@@ -1,1 +0,0 @@
-trapipe receive -t "{{ .Message.ChannelID }} {{ .Message.PlainText }}" | grep --line-buffered "$2" | while read -r cid _ args; do "$2" "$args" | trapipe send --channel-id "$cid"; done
