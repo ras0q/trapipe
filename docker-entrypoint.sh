0354b4c4ca98261cec9534e69896f97550292705
Kira Kawai <66677201+ras0q@users.noreply.github.com>
2026-01-14 22:28:43 +0900

    refactor: pass channelID (#17)
    Co-authored-by: ras0q <ras0q@users.noreply.github.com>

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/sh -eu
 
-command -v jq >/dev/null || { echo "Error: jq command not found." >&2; exit 1; }
 command -v trapipe >/dev/null || { echo "Error: trapipe command not found." >&2; exit 1; }
 [ -z "${TRAQ_BOT_ACCESS_TOKEN}" ] && { echo "Error: TRAQ_BOT_ACCESS_TOKEN is not set." >&2; exit 1; }
 [ $# -eq 0 ] && { echo "Error: No command provided to execute." >&2; exit 1; }
 
-exec trapipe receive -t "{{ json . }}" |
+exec trapipe receive -t "{{ .Message.ChannelID }} {{ json . }}" |
   # $payload is a MESSAGE_CREATED event payload
   # Ref: https://bot-console.trap.jp/docs/bot/events/message
-  while read -r payload; do
-    channel_id=$(echo "$payload" | jq -r '.message.channelId')
+  while read -r channel_id payload; do
     $@ "$payload" | trapipe send --channel-id "$channel_id"
   done
